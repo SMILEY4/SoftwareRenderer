9e5a753ca2ac7e7d1217e2c76118be5705f49db6
Lukas Rügner <ruegnerlukas@gmail.com>
2018-01-22 21:03:38 +0100

    Dop Renderer

--- src/renderer.h
+++ src/renderer.h
@@ -1,19 +1,6 @@
 #ifndef SOFTWARERENDERER_RENDERER_H
 #define SOFTWARERENDERER_RENDERER_H
 
-#include "model.h"
-#include "bitmap.h"
-#include "camera.h"
-
-
-camera_t srCamera;
-
-
-void srRender(bitmap_t *bitmap, model_t *model);
-
-void srRenderWireframe(bitmap_t *bitmap, model_t *model);
-
-void srInit(float width, float height);
 
 
 #endif
